655e055fe0844da605c4e9e7c1bd671ad857a70b
Nico Borner <48905028+nicoborner@users.noreply.github.com>
2021-08-06 17:01:48 +0200

    test

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -76,26 +76,3 @@ catch {
     write-host "Error installing Acrobat Reader: $ErrorMessage"
 }
 #endregion
-
-
-#region Time Zone Redirection
-$Name = "fEnableTimeZoneRedirection"
-$value = "1"
-# Add Registry value
-try {
-    New-ItemProperty -ErrorAction Stop -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name $name -Value $value -PropertyType DWORD -Force
-    if ((Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services").PSObject.Properties.Name -contains $name) {
-        Write-log "Added time zone redirection registry key"
-        Write-host "Added time zone redirection registry key"
-    }
-    else {
-        write-log "Error locating the Teams registry key"
-        write-host "Error locating the Teams registry key"
-    }
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error adding teams registry KEY: $ErrorMessage"
-    write-host "Error adding teams registry KEY: $ErrorMessage"
-}
-#endregion
