73f02a6955747cf69ecf83eb40ba114f7d0a13dd
Nico Borner <48905028+nicoborner@users.noreply.github.com>
2021-08-06 16:26:19 +0200

    output while installing apps

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -25,14 +25,17 @@ try {
     Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\7z1900-x64.msi', '/qn'
     if (Test-Path "C:\Program Files\7-Zip\7zFM.exe") {
         Write-Log "7-Zip has been installed"
+        Write-Host "7-Zip has been installed"
     }
     else {
         write-log "Error locating the 7-Zip executable"
+        write-host "Error locating the 7-Zip executable"
     }
 }
 catch {
     $ErrorMessage = $_.Exception.message
     write-log "Error installing 7-Zip: $ErrorMessage"
+    write-host "Error installing 7-Zip: $ErrorMessage"
 }
 #endregion
 
@@ -41,14 +44,17 @@ try {
     Start-Process -filepath 'c:\temp\npp.8.1.2.Installer.x64.exe' -Wait -ErrorAction Stop -ArgumentList '/S'
     if (Test-Path "C:\Program Files\Notepad++\notepad++.exe") {
         Write-Log "Notepad++ has been installed"
+        Write-host "Notepad++ has been installed"
     }
     else {
         write-log "Error locating the Notepad++ executable"
+        write-host "Error locating the Notepad++ executable"
     }
 }
 catch {
     $ErrorMessage = $_.Exception.message
     write-log "Error installing Notepad++: $ErrorMessage"
+    write-host "Error installing Notepad++: $ErrorMessage"
 }
 #endregion
 
@@ -57,28 +63,20 @@ try {
     Start-Process -filepath 'c:\temp\AcroRdrDC2100520058_en_US.exe' -Wait -ErrorAction Stop -ArgumentList '/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES'
     if (Test-Path "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe") {
         Write-Log "Acrobat Reader has been installed"
+        Write-host "Acrobat Reader has been installed"
     }
     else {
         write-log "Error locating the Acrobat Reader executable"
+        write-host "Error locating the Acrobat Reader executable"
     }
 }
 catch {
     $ErrorMessage = $_.Exception.message
     write-log "Error installing Acrobat Reader: $ErrorMessage"
+    write-host "Error installing Acrobat Reader: $ErrorMessage"
 }
 #endregion
 
-#region Sysprep Fix
-# Fix for first login delays due to Windows Module Installer
-try {
-    ((Get-Content -path C:\DeprovisioningScript.ps1 -Raw) -replace 'Sysprep.exe /oobe /generalize /quiet /quit', 'Sysprep.exe /oobe /generalize /quit /mode:vm' ) | Set-Content -Path C:\DeprovisioningScript.ps1
-    write-log "Sysprep Mode:VM fix applied"
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error updating script: $ErrorMessage"
-}
-#endregion
 
 #region Time Zone Redirection
 $Name = "fEnableTimeZoneRedirection"
@@ -88,13 +86,16 @@ try {
     New-ItemProperty -ErrorAction Stop -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name $name -Value $value -PropertyType DWORD -Force
     if ((Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services").PSObject.Properties.Name -contains $name) {
         Write-log "Added time zone redirection registry key"
+        Write-host "Added time zone redirection registry key"
     }
     else {
         write-log "Error locating the Teams registry key"
+        write-host "Error locating the Teams registry key"
     }
 }
 catch {
     $ErrorMessage = $_.Exception.message
     write-log "Error adding teams registry KEY: $ErrorMessage"
+    write-host "Error adding teams registry KEY: $ErrorMessage"
 }
 #endregion
